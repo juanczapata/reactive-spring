3d4a934ac5f40cba6d88b898eb399315d3857f29
juanczapata <jcarczapata@gmail.com>
2022-08-23 22:21:02 -0500

    Add tests

--- src/test/java/com/jcczdev/reactive1/repository/PersonRepositoryImplTest.java
+++ src/test/java/com/jcczdev/reactive1/repository/PersonRepositoryImplTest.java
@@ -3,7 +3,10 @@ package com.jcczdev.reactive1.repository;
 import com.jcczdev.reactive1.domain.Person;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -34,5 +37,46 @@ class PersonRepositoryImplTest {
         Mono<Person> personMono = repository.getById(1);
         personMono.map(person -> person.getFirstName())
                 .subscribe(firstName -> System.out.println("From function: ".concat(firstName)));
+    }
+
+    @Test
+    void getAllIdUsingBlockFirst() {
+        Flux<Person> personFlux = repository.getAll();
+        System.out.println("Flux blockFirst: ".concat(personFlux.blockFirst().toString()));
+    }
+
+    @Test
+    void getAllUsingSubscribe() {
+        Flux<Person> personFlux = repository.getAll();
+        personFlux.subscribe(person -> {
+            System.out.println("flux subscribe: ".concat(person.toString()));
+        });
+    }
+
+    @Test
+    void getAllUsingCollectList() {
+        Flux<Person> personFlux = repository.getAll();
+        Mono<List<Person>> listMono = personFlux.collectList();
+        listMono.subscribe(people -> {
+            people.forEach(person -> System.out.println("Flux with collectList: ".concat(person.toString())));
+        });
+    }
+
+    @Test
+    void testFluxPersonById() {
+        Flux<Person> personFlux = repository.getAll();
+        final Integer id = 1;
+        Mono<Person> monoPerson = personFlux.filter(person -> person.getId() == id).next();
+        monoPerson.subscribe(person -> System.out.println("Flux filtering: ".concat(person.toString())));
+    }
+
+    @Test
+    void testFluxPersonHandlingException() {
+        Flux<Person> personFlux = repository.getAll();
+        final Integer id = 20;
+        Mono<Person> monoPerson = personFlux.filter(person -> person.getId() == id).single();
+        monoPerson.doOnError(throwable -> System.out.println("Exception Thrown"))
+                .onErrorReturn(Person.builder().build())
+                .subscribe(person -> System.out.println("Wihtout exception: ".concat(person.toString())));
     }
 }
